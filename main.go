d019cd976bd784554f1c979b2768214eab781a5f
Jeremy Willden <jeremy@constellationlabs.com>
2020-01-27 11:16:27 -0700

    added a kickstart TOKEN message from the master device to simplify the process of starting a token loop

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 	"constellationlabs.com/iotloop/ledcontrol"
 )
 
+const portnumber = 2468
+
 func main() {
 	fmt.Println(iotnet.Myname())
+	if "pi-cluster-00" == iotnet.Myname() {
+		// start the whole loop by sending an initial token
+		iotnet.PassToken(iotnet.GetNextHop(), portnumber)
+	}
 	c := make(chan bool)
-	go iotnet.ListenForToken(2468, c)
+	go iotnet.ListenForToken(portnumber, c)
 	timeout := make(chan bool, 1)
 	go waittimeout(timeout)
 	running := true
@@ -20,9 +26,9 @@ func main() {
 		case rxok := <-c:
 			if rxok {
 				fmt.Println("token received, passing to " + iotnet.GetNextHop())
-				newcolors := []uint32{0x110000, 0x001100} // using GGRRBB color ordering
+				newcolors := []uint32{0x440000, 0x004400, 0x000044, 0x222222} // using GGRRBB color ordering
 				ledcontrol.SetLeds(newcolors)
-				if iotnet.PassToken(iotnet.GetNextHop(), 2468) {
+				if iotnet.PassToken(iotnet.GetNextHop(), portnumber) {
 					fmt.Println("token passed")
 					offcolors := []uint32{0x000000, 0x000000} // using GGRRBB color ordering
 					ledcontrol.SetLeds(offcolors)
@@ -36,6 +42,6 @@ func main() {
 }
 
 func waittimeout(timeoutchan chan bool) {
-	time.Sleep(10 * time.Second)
+	time.Sleep(30 * time.Second)
 	timeoutchan <- true
 }
