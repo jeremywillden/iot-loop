03992a032027960d0b6cbaddccc9650d904a0eb0
Jeremy Willden <jeremy@constellationlabs.com>
2020-01-27 09:10:36 -0700

    got the two main blocks in place for receiving and sending tokens, now to test them

--- iotnet/iotnet.go
+++ iotnet/iotnet.go
@@ -28,3 +28,32 @@ func PassToken(passto string, portnum int) (passed bool) {
 	}
 	return false
 }
+
+// ListenForToken opens a connection, returning a boolean true in a channel when the token is received
+func ListenForToken(portnum int, tokenchan chan bool) {
+	running := true
+	portstr := strconv.Itoa(portnum)
+	rxsocket, err := net.Listen("tcp", ":"+portstr)
+	if err != nil {
+		running = false
+	}
+	for running {
+		conn, err := rxsocket.Accept()
+		if err != nil {
+			running = false
+		}
+		go receiveToken(conn, tokenchan)
+	}
+}
+
+func receiveToken(conn net.Conn, tokenchan chan bool) {
+	buffer := make([]byte, 1024)
+	len, err := conn.Read(buffer)
+	if (err == nil) && (len < 128) {
+		conn.Write([]byte("OK\r\n"))
+		if strings.Contains(string(buffer), "TOKEN") {
+			tokenchan <- true
+		}
+	}
+	tokenchan <- false
+}
--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 
-	"constellationlabs.com/iotloop/iotnet"
+	iotloop "constellationlabs.com/iotloop/iotnet"
 )
 
 func main() {
 	fmt.Println(iotloop.Hello())
+	c := make(chan bool)
+	go iotloop.ListenForToken(2468, c)
+	running := true
+	for running {
+		select {
+		case rxok := <-c:
+			if rxok {
+				fmt.Println("token received")
+			} else {
+				fmt.Println("problem receiveing token")
+			}
+		}
+	}
 }
